f0abf532d8e91512c412986732cf2b75f074a28a
nathan <imanasianninja@gmail.com>
2015-08-22 10:16:48 -0700

    heap

---
+++ src/PadLogic/Utilities/Heap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PadLogic.Utilities
+{
+    public class Heap<T> : List<T>
+    {
+        private int m_size;
+
+        public Heap(int size)
+            : base(size)
+        {
+            m_size = size;
+        }
+
+        public void Enqueue(T item)
+        {
+             
+        }
+    }
+}
